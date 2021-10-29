7b9d50c6e0b76a83ebefcf363fb90fa58fb01ac6
Ms.BL43K <75617768+YeMinAung2004@users.noreply.github.com>
2021-10-29 21:05:20 +0630

    Update warning.sh

--- warning.sh
+++ warning.sh
@@ -30,25 +30,5 @@ echo
 elif [[ $Z == n ]];then
 exit
 echo "FROM DEVELOPER=>GOOD CHOICE NOW TRY INSTALL OTHER LINUX :) "
-elif [[ $Z == y ]];then
-echo "INSTALLATION START IN"
-sleep 1
-echo "5"
-sleep 1
-echo "4"
-sleep 1
-echo "3"
-sleep 1
-echo "2"
-sleep 1
-echo "1"
-sleep 0.5
-echo "Started"
-echo
-pkg install wget openssl-tool proot tar -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Arch/armhf/arch.sh && bash arch.sh && chmod 755 additional.sh && ./additional.sh
-figlet INSTALLATION FINISHED
-echo
-echo "Note=>Start using with ./start-arch.sh"
-echo "Note=>If the Script was damage some error you can Open issue on my github account"
-echo
+
 fi
